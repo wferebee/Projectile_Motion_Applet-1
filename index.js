f40f2832fbc157d3f7f40820c16b4cc7e1d9a21f
DP3PO <williampferebee@gmail.com>
2020-05-02 19:55:03 -0400

    changed the equation for the power output using cubic regression and cleared the console

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var context;
 var canvas = document.getElementById('myCanvas');
 context = canvas.getContext('2d');
-
 var startTime, endTime;
+
 var myRectangle ={
     x: 0,
     y: canvas.height,
@@ -38,7 +38,7 @@ function animate() {
     start();
     //start = {};
     setInterval(draw, 50);
-    console.log(window.innerWidth)
+    //.log(window.innerWidth)
 }
 
 var slider = document.getElementById("myRange");
@@ -48,8 +48,8 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
   myRectangle.theta = this.value * (Math.PI/180);
-  console.log(slider.value);
-  console.log("this is theta: " + myRectangle.theta)
+  //console.log(slider.value);
+  //console.log("this is theta: " + myRectangle.theta)
 }
 
 var sliderPower = document.getElementById("myPower");
@@ -59,7 +59,16 @@ outputPower.innerHTML = sliderPower.value;
 sliderPower.oninput = function() {
   outputPower.innerHTML = this.value;
   //myRectangle.v = this.value/(4.132231404);
-  myRectangle.v = this.value / Math.pow(3, 2) + 13;
+  //myRectangle.v = this.value / 4.132
+  // myRectangle.v = this.value / Math.pow(3, 2) + 13; // need to fix this equation* * Fixed this equation on the one below.
+  // plugged in the coordinates (0,0),(25,11.1),(50,16.9),(75,19.9),(100,24.2) into a graph to find a line of best fit.
+  // using cubic regression, I found the below equation to giv me the values i wanted from associated power levels on the slider.
+
+  myRectangle.v = (0.0000352 * Math.pow((this.value), 3)) + (-0.00715429 * Math.pow((this.value), 2)) + (0.605429 * (this.value)) + (-0.0228571);  
   //console.log(sliderPower.value);
   //console.log("this is Power: " + myRectangle.v)
 }
+
+
+
+
