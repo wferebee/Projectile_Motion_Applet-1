19a142a8f5f164b3b4d991c9cba4262c37fde4e3
William Ferebee <williampferebee@gmail.com>
2024-02-22 01:43:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var context;
-var canvas = document.getElementById('myCanvas');
-context = canvas.getContext('2d');
-var startTime, endTime;
+var canvas, context, button, startTime, endTime, slider, sliderPower, output, outputPower;
 
-var myRectangle ={
+canvas = document.getElementById('myCanvas');
+context = canvas.getContext('2d');
+
+button = document.getElementById("mybtn");
+button.addEventListener("click", animate)
+
+angleSlider = document.getElementById("myRange");
+angleSlider.oninput = captureAngleSliderInput;
+
+angleOutput = document.getElementById("demo");
+angleOutput.innerHTML = angleSlider.value;
+
+powerSlider = document.getElementById("myPower");
+powerSlider.oninput = capturePowerSliderInput;
+
+powerOutput = document.getElementById("demoPower");
+powerOutput.innerHTML = powerSlider.value;
+
+var myRectangle = {
     x: 0,
     y: canvas.height,
     yi: canvas.height,
@@ -14,60 +29,57 @@ var myRectangle ={
 
 function draw () {
     endTime = new Date();
-    var timeDiff = endTime - startTime; //in ms
-    timeDiff /= 50;
+    var timeDiff = (endTime - startTime) / 50; //in ms
     var s = Math.round(timeDiff);
+    
     myRectangle.y = myRectangle.yi - (myRectangle.v*(s)*(Math.sin(myRectangle.theta))) +(.98*Math.pow(s, 2));
+    
     myRectangle.x = (myRectangle.v*(s))*Math.cos(myRectangle.theta);
+    
     context.clearRect(0,0, window.innerWidth,window.innerHeight);
     context.beginPath();
+    
     context.arc(myRectangle.x, myRectangle.y, myRectangle.radius, 0, 2*Math.PI, true);
+    
     context.fillStyle= "green";
     context.fill();
 }
 
 function start() {
     startTime = new Date();
-  };
-
-document.getElementById("mybtn").addEventListener("click", animate);
+};
 
 function animate() {
     var canvas = document.getElementById('myCanvas');
     context = canvas.getContext('2d');
     start();
-    //start = {};
     setInterval(draw, 50);
-    //.log(window.innerWidth)
 }
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
+function captureAngleSliderInput(){
   output.innerHTML = this.value;
   myRectangle.theta = this.value * (Math.PI/180);
-  //console.log(slider.value);
-  //console.log("this is theta: " + myRectangle.theta)
 }
 
-var sliderPower = document.getElementById("myPower");
-var outputPower = document.getElementById("demoPower");
-outputPower.innerHTML = sliderPower.value;
-
-sliderPower.oninput = function() {
-  outputPower.innerHTML = this.value;
-  //myRectangle.v = this.value/(4.132231404);
-  //myRectangle.v = this.value / 4.132
-  // myRectangle.v = this.value / Math.pow(3, 2) + 13; // need to fix this equation* * Fixed this equation on the one below.
-  // plugged in the coordinates (0,0),(25,11.1),(50,16.9),(75,19.9),(100,24.2) into a graph to find a line of best fit.
-  // using cubic regression, I found the below equation to giv me the values i wanted from associated power levels on the slider.
-
-  myRectangle.v = (0.0000352 * Math.pow((this.value), 3)) + (-0.00715429 * Math.pow((this.value), 2)) + (0.605429 * (this.value)) + (-0.0228571);  
-  //console.log(sliderPower.value);
-  //console.log("this is Power: " + myRectangle.v)
+function capturePowerSliderInput(){
+ outputPower.innerHTML = this.value;
+  myRectangle.v = (0.0000352 * Math.pow((this.value), 3)) + (-0.00715429 * Math.pow((this.value), 2)) + (0.605429 * (this.value)) + (-0.0228571); 
 }
+
+  // Ideally when the angle is set to 90 degrees, and the power level is set to 100%, the projectile should just ouch the top of the rectangle.
+  //    And when the angle is set to 45 degrees with a power level of 100%, the projetile should just otuch the far bottom corner of the rectangle. 
+  //    To do this, I had to play around with the values, but that didnt yield great results.
+
+  // Early Attempts at getting the powerlevel and angle right -
+  // myRectangle.v = this.value/(4.132231404);
+  // myRectangle.v = this.value / 4.132;
+  // myRectangle.v = this.value / Math.pow(3, 2) + 13;
+
+  // SOLVE -
+  // I plugged in the coordinates shown below into a graph in order to find a line of best fit, and using cubic regression,
+  // I came up with this equation which gave the best values for the associated angles and power levels I desired.
+  //        (0,0),(25,11.1),(50,16.9),(75,19.9),(100,24.2) 
+
 
 
 
